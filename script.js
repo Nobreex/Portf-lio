254823c39567a84d81a59019fbb12fbcd9ca6b4d
Nobreex <diogo.nobre.23012006@gmail.com>
2025-01-02 01:49:46 -0300

    commit

--- script.js
+++ script.js
@@ -7,18 +7,7 @@ menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
 
-function showloading(event) {
-  event.preventDefault(); // Impede o redirecionamento imediato do link
-
-  // Captura a URL do link
-  const targetUrl = event.currentTarget.getAttribute('href');
-
-  // Mostra uma mensagem de carregamento
+function showLoading(event) {
+  event.preventDefault(); // Evita a navegação padrão do link
   alert("Carregando...");
-
-  // Aguarda 1 segundo antes de redirecionar
-  setTimeout(() => {
-      window.location.href = targetUrl; // Redireciona para a URL do link
-  }, 1000); // Tempo de espera em milissegundos (1 segundo)
 }
-
